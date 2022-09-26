30c93bce35aa75b9147dc9cb4bd2e05cb781190b
Moti <dinsamoti@gmail.com>
2022-09-26 09:41:04 +0300

    bubble sort done

---
+++ hackerrank/Sorting Bubble Sort.py
@@ -0,0 +1,14 @@
+def countSwaps(a):
+    swapCount = 0
+    for i in range(len(a)-1):
+        for j in range(len(a)-1):
+            if a[j] > a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
+                swapCount+=1
+    print(f'Array is sorted in {swapCount} swaps.')
+    print(f'First Element: {a[0]}')
+    print(f'Last Element: {a[len(a)-1]}')
+
+
+
+countSwaps([3,6,4,1,2])
